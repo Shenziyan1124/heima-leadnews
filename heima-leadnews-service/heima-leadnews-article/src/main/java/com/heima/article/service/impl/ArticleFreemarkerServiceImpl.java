7f166a68a32c44c172a15811ab049d3db300a11e
shenziyan <shenziyan@hydsoft.com>
2026-08-28 13:56:55 +0800

    feat(search): day07-实现文章搜索功能并集成Elasticsearch
    - 新增ApArticleMapper接口及XML映射文件实现文章列表加载
    - 添加Elasticsearch配置类支持ES连接管理
    - 实现文章数据批量导入ES的功能测试类
    - 在文章服务中添加ES索引同步的消息发送逻辑
    - 创建search微服务模块并配置相关依赖
    - 实现基于关键词的ES文章搜索功能
    - 添加搜索结果高亮显示和分页功能
    - 实现Kafka消息监听器同步文章到ES索引
    - 定义搜索相关的DTO和VO数据传输对象
    - 配置搜索服务的Nacos注册中心连接

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ArticleFreemarkerServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ArticleFreemarkerServiceImpl.java
@@ -1,18 +1,23 @@
 package com.heima.article.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.heima.article.service.ApArticleService;
 import com.heima.article.service.ArticleFreemarkerService;
 import com.heima.file.service.FileStorageService;
 import com.heima.model.article.pojos.ApArticle;
+import com.heima.model.search.vos.SearchArticleVo;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.net.nntp.Article;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +27,8 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
+
+import static com.heima.common.constants.ArticleConstants.ARTICLE_ES_SYNC_TOPIC;
 
 @Service
 @Slf4j
@@ -72,7 +79,30 @@ public class ArticleFreemarkerServiceImpl implements ArticleFreemarkerService {
             apArticleService.update(Wrappers.<ApArticle>lambdaUpdate()
                     .eq(ApArticle::getId, apArticle.getId())
                     .set(ApArticle::getStaticUrl, path));
+
+
+            // 5. 发送消息,同步es index索引
+            createArticleESIndex(apArticle,content,path);
         }
 
+    }
+
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    /**
+     * 创建文章ES索引
+     * @param apArticle
+     * @param content
+     * @param path
+     */
+    private void createArticleESIndex(ApArticle apArticle, String content, String path) {
+        SearchArticleVo vo = new SearchArticleVo();
+        BeanUtils.copyProperties(apArticle, vo);
+        vo.setContent(content);
+        vo.setStaticUrl(path);
+
+        kafkaTemplate.send(ARTICLE_ES_SYNC_TOPIC, JSON.toJSONString(vo));
     }
 }
---
+++ heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/service/impl/ArticleSearchServiceImpl.java
@@ -0,0 +1,98 @@
+package com.heima.search.service.impl;
+
+
+import com.alibaba.fastjson.JSON;
+import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.common.enums.AppHttpCodeEnum;
+import com.heima.model.search.dtos.UserSearchDto;
+import com.heima.search.service.ArticleSearchService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.net.nntp.Article;
+import org.apache.lucene.util.QueryBuilder;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.text.Text;
+import org.elasticsearch.index.query.*;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.SortOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+@Service
+@Slf4j
+public class ArticleSearchServiceImpl implements ArticleSearchService {
+
+    @Autowired
+    private RestHighLevelClient client;
+
+    @Override
+    public ResponseResult search(UserSearchDto dto) throws IOException {
+        // 1.校验参数
+        if (dto == null || StringUtils.isBlank(dto.getSearchWords())) {
+            return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
+        }
+
+        // 2.设置查询条件
+        SearchRequest searchRequest = new SearchRequest("app_info_article");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+        // 布尔查询
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+
+        // 2.1关键字分词之后的查询
+        QueryStringQueryBuilder queryStringQueryBuilder = QueryBuilders.queryStringQuery(dto.getSearchWords())
+                .field("title").field("content").defaultOperator(Operator.OR);
+        boolQueryBuilder.must(queryStringQueryBuilder);
+
+        // 2.2查询小于minBehotTime的数据
+        RangeQueryBuilder publishTime = QueryBuilders.rangeQuery("publishTime").lt(dto.getMinBehotTime().getTime());
+        boolQueryBuilder.filter(publishTime);
+
+        // 2.3分页查询
+        searchSourceBuilder.from(0);
+        searchSourceBuilder.size(dto.getPageSize());
+
+        // 2.4按照发布时间倒叙查询
+        searchSourceBuilder.sort("publishTime", SortOrder.DESC);
+
+        // 2.5设置高亮title
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.field("title");
+        highlightBuilder.preTags("<font style='color: red; font-size: inherit;'>");
+        highlightBuilder.postTags("</font>");
+        searchSourceBuilder.highlighter(highlightBuilder);
+
+        searchSourceBuilder.query(boolQueryBuilder);
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        // 3.结果封装返回
+        List<Map> list = new ArrayList<>();
+        SearchHit[] hits = searchResponse.getHits().getHits();
+        for (SearchHit hit : hits){
+            String json = hit.getSourceAsString();
+            Map map = JSON.parseObject(json, Map.class);
+            if (hit.getHighlightFields() != null && !hit.getHighlightFields().isEmpty()){
+                Text[] titles = hit.getHighlightFields().get("title").fragments();
+                String title = StringUtils.join(titles);
+                map.put("h_title", title);
+            }else {
+                map.put("h_title", map.get("title"));
+            }
+            list.add(map);
+        }
+        return ResponseResult.okResult(list);
+    }
+}
